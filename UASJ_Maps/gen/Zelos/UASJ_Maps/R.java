2797d5041e85352515c5cbb71c312db3c0b45d49
David <langner.eric@gmx.net>
2011-12-18 20:43:00 +0100

    David: GUI und Buttons vorrangetrieben. Problem: IndexOutOfBoundsException bei Zugriff auf Routenberechnung
    unused Projects deleted

--- UASJ_Maps/gen/Zelos/UASJ_Maps/R.java
+++ UASJ_Maps/gen/Zelos/UASJ_Maps/R.java
@@ -15,23 +15,41 @@ public final class R {
     }
     public static final class id {
         public static final int but_Campus1=0x7f050005;
-        public static final int but_Campus3=0x7f050010;
+        public static final int but_Campus3=0x7f050011;
+        public static final int but_Campus5=0x7f05001d;
+        public static final int but_Campus7=0x7f050023;
+        public static final int but_Go2=0x7f05000a;
+        public static final int but_Go4=0x7f050014;
         public static final int but_Routing1=0x7f050003;
-        public static final int but_Routing3=0x7f05000f;
+        public static final int but_Routing3=0x7f050010;
+        public static final int but_Routing5=0x7f05001c;
+        public static final int but_Routing7=0x7f050022;
         public static final int but_ShowPosition1=0x7f050002;
         public static final int but_options1=0x7f050006;
-        public static final int button1=0x7f050011;
-        public static final int floor_minus3=0x7f05000e;
-        public static final int floor_plus3=0x7f05000d;
-        public static final int go2=0x7f05000a;
+        public static final int editText2=0x7f05000b;
+        public static final int editText41=0x7f050016;
+        public static final int editText42=0x7f050017;
+        public static final int floor_minus3=0x7f05000f;
+        public static final int floor_minus5=0x7f05001b;
+        public static final int floor_minus7=0x7f050021;
+        public static final int floor_plus3=0x7f05000e;
+        public static final int floor_plus5=0x7f05001a;
+        public static final int floor_plus7=0x7f050020;
         public static final int linearLayout11=0x7f050001;
         public static final int linearLayout12=0x7f050004;
         public static final int relativeLayout2=0x7f050007;
-        public static final int relativeLayout3=0x7f05000b;
+        public static final int relativeLayout3=0x7f05000c;
+        public static final int relativeLayout5=0x7f050018;
         public static final int tableLayout1=0x7f050000;
         public static final int textView21=0x7f050008;
         public static final int textView22=0x7f050009;
-        public static final int view3=0x7f05000c;
+        public static final int textView41=0x7f050012;
+        public static final int textView42=0x7f050015;
+        public static final int textView43=0x7f050013;
+        public static final int textView5=0x7f05001e;
+        public static final int view3=0x7f05000d;
+        public static final int view5=0x7f050019;
+        public static final int view7=0x7f05001f;
     }
     public static final class layout {
         public static final int state_1=0x7f030000;
--- UASJ_Maps/src/Zelos/UASJ_Maps/GUI.java
+++ UASJ_Maps/src/Zelos/UASJ_Maps/GUI.java
@@ -12,87 +12,141 @@ import android.widget.*;
 
 public class GUI extends Activity {
 
-	private Pathfinding RB;
-	private ArrayList<ArrayList<Node>> Route;
-	private GraphicalOutput go; // beinhaltet grafische Darstellung der Testumgebung
+	private GraphicalOutput output; // beinhaltet grafische Darstellung der Testumgebung
 	private CompassListener cl; // beinhaltet Lagesensor
 
 	/*
 	 * TODO-liste und was so funktioniert:
 	 * state1: Main menu: funktioniert
 	 * state2: ShowPosition menu: auswahldinger fehlen
-	 * state3: ShowPosition: onclicklistener fehlen
+	 * state3: ShowPosition view: onclicklistener fehlen
 	 * state4: Routing menu: ein button ist, der rest fehlt
-	 * state5: Routing: ohne funktion
+	 * state5: Routing view: ohne funktion
 	 * state6: Options: ohne funktion
-	 * state7: Campus: ohne funktion
+	 * state7: Campus view: ohne funktion
 	 */
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) { // App wird gestartet
 		super.onCreate(savedInstanceState); // onCreate von Activity
 		cl = new CompassListener(); // neue Instanz zur Initialisierung des Sensors
-//		go = new GraphicalOutput(getApplicationContext()); TODO enable wenn Konstruktor vorhanden
-		setContentView(R.layout.state_1); // state_1.xml anzeigen
+//		output = new GraphicalOutput(getApplicationContext()); TODO enable wenn Konstruktor vorhanden
+		launch_state_1(); // Hauptmenu anzeigen
+		// TODO begin löschen: routenberechnung, weil das hier ja showposition ist
+		Pathfinding RB = new Pathfinding();
+		RB.compute_Path(0, 0);
+		ArrayList<ArrayList<Node>> Route = RB.getPath();
+		output = new GraphicalOutput(getApplicationContext(), Route);
+		// ende löschen
 	}
 	
 	@Override
-	public void onStop() {
-		cl.onStop(); // stoppt Ausführung des Magnet_Sensors
-		super.onStop(); // onStop von Activity
+	public void onPause() { // App wird beendet
+		cl.onPause(); // stoppt Ausführung des Magnetsensors
+		super.onPause(); // onPause von Activity
+	}
+	
+	private void launch_state_1() {
+		setContentView(R.layout.state_1); // state_1.xml anzeigen
+		
+		// OnClickListener für die Buttons:
+		findViewById(R.id.but_ShowPosition1).setOnClickListener(new OnClickListener() {
+			// OnClickListener für ShowPosition
+			public void onClick(View v) {
+				launch_state_2(); // ShowPositions menu
+			}
+		});
+		
+		findViewById(R.id.but_Routing1).setOnClickListener(new OnClickListener() {
+			// OnClickListener für Routing
+			public void onClick(View v) {
+				launch_state_4(); // Routing menu
+			}
+		});
+		
+		findViewById(R.id.but_Campus1).setOnClickListener(new OnClickListener() {
+			// OnClickListener für Campus
+			public void onClick(View v) {
+				launch_state_7(); // Campus view
+			}
+		});
+		
+		findViewById(R.id.but_options1).setOnClickListener(new OnClickListener() {
+			// OnClickListener für Options
+			public void onClick(View v) {
+				launch_state_6(); // Options
+			}
+		});
 	}
 
-	public void launch_state_2(final View view) {
-		setContentView(R.layout.state_2);
+	private void launch_state_2() {
+		setContentView(R.layout.state_2); // state_2.xml anzeigen
+		findViewById(R.id.but_Go2).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				EditText roomInput = (EditText) findViewById(R.id.editText2); // TODO abfangen von fehleingaben
+				Pathfinding pf = new Pathfinding();
+				pf.compute_Path(Integer.parseInt(roomInput.getText().toString()), Integer.parseInt(roomInput.getText().toString()));
+				System.out.println(pf.getPath());
+				System.out.println(pf.getPath().get(0)); // TODO das ding ist leer --> nils fragen
+				launch_state_3(pf.getPath().get(0).get(0)); // ShowPosition view
+			}
+		});
 		// TODO wheel oder wheelpicker
 	}
+ 
+	private void launch_state_3(Node position) {
+		output.set_state_position(position);
 
-	public void launch_state_3(final View view) {
-		// TODO begin löschen: routenberechnung, weil das hier ja showposition ist
-		RB = new Pathfinding();
-		RB.compute_Path(0, 7);
-		Route = RB.getPath();
-		go = new GraphicalOutput(getApplicationContext(), Route);
-		// ende löschen --> setstatus(x);
-
-		setContentView(R.layout.state_3);
+		setContentView(R.layout.state_3); // state_3.xml anzeigen
+		// Elemente der Anzeige holen, damit sie bearbeitet werden können:
 		final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayout3);
-		View v = findViewById(R.id.view3);
-		Button fminus = (Button) findViewById(R.id.floor_minus3);
-		Button fplus = (Button) findViewById(R.id.floor_plus3);
+		View backgroundView = findViewById(R.id.view3);
+		final Button fminus = (Button) findViewById(R.id.floor_minus3);
+		final Button fplus = (Button) findViewById(R.id.floor_plus3);
 		Button routing = (Button) findViewById(R.id.but_Routing3);
 		Button campus = (Button) findViewById(R.id.but_Campus3);
 
-		v = go;
+		backgroundView = output; // grafische Ausgabe als Hintergrund setzen
 
+		// OnClickListener für die Buttons:
 		fminus.setOnClickListener(new OnClickListener() {
+			// OnClickListener für F-
 			public void onClick(View v) {
-				// TODO
-				go.invalidate();
+				fplus.setEnabled(true); // falls F- auf oberstem Stockwerk gedrückt wurde, F+ wieder enablen
+				if (-1 == output.set_floor(0)) // wenn -1 returned wird, unterstes Stockwerk erreicht
+					fminus.setEnabled(false); // F- disabeln
+				output.invalidate(); // redraw
 			}
 		});
 
 		fplus.setOnClickListener(new OnClickListener() {
+			// OnClickListener für F+
 			public void onClick(View v) {
-				// TODO
-				go.invalidate();
+				fminus.setEnabled(true); // falls F+ auf unterstem Stockwerk gedrückt wurde, F- wieder enablen
+				if (1 == output.set_floor(1)) // wenn 1 returned wird, oberstes Stockwerk erreicht
+					fplus.setEnabled(false); // F+ disabeln
+				output.invalidate(); // redraw
 			}
 		});
 
 		routing.setOnClickListener(new OnClickListener() {
+			// OnClickListener für Routing
 			public void onClick(View v) {
-				launch_state_4(view);
+				launch_state_4();
 			}
 		});
 
 		campus.setOnClickListener(new OnClickListener() {
+			// OnClickListener für Campus
 			public void onClick(View v) {
-				launch_state_7(view);
+				output.set_floor(2);
+				output.invalidate(); // redraw
 			}
 		});
 
-		rl.removeAllViews();
-		rl.addView(v);
+		rl.removeAllViews(); // zunächst müssen alle Views entfernt werden, da sie sonst doppelt vorhanden sind
+		// danach "von hinten nach vorne" die geänderten Elemente wieder hinzufügen, ansonsten würde die Route über den Button dargestellt werden
+		rl.addView(backgroundView);
 		rl.addView(fminus);
 		rl.addView(fplus);
 		rl.addView(routing);
@@ -102,22 +156,89 @@ public class GUI extends Activity {
 		// TODO vielleicht lieber in die override, aber momentan gehts auch ohne, da sich die app bei resume noch nicht den status gemerkt hat, also auch onResume und was es sonst noch so gibt mal overriden
 	}
 
-	public void launch_state_4(final View view) {
+	private void launch_state_4() {
 		setContentView(R.layout.state_4);
+		findViewById(R.id.but_Go4).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				EditText roomInput1 = (EditText) findViewById(R.id.editText41); // TODO abfangen von fehleingaben
+				EditText roomInput2 = (EditText) findViewById(R.id.editText42); // TODO abfangen von fehleingaben
+				Pathfinding pf = new Pathfinding();
+				pf.compute_Path(Integer.parseInt(roomInput1.getText().toString()), Integer.parseInt(roomInput2.getText().toString()));
+				System.out.println(pf.getPath()); // TODO das ding ist leer --> nils fragen
+				launch_state_5(pf.getPath());
+			}
+		});
 		// TODO wheel oder wheelpicker
 	}
 
-	public void launch_state_5(final View view) {
-		setContentView(R.layout.state_5);
-		// TODO
+	private void launch_state_5(ArrayList<ArrayList<Node>> path) {
+		output.set_state_path(path);
+		
+		setContentView(R.layout.state_5); // state_5.xml anzeigen
+		// Elemente der Anzeige holen, damit sie bearbeitet werden können:
+		final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayout5);
+		View backgroundView = findViewById(R.id.view5);
+		final Button fminus = (Button) findViewById(R.id.floor_minus5);
+		final Button fplus = (Button) findViewById(R.id.floor_plus5);
+		Button routing = (Button) findViewById(R.id.but_Routing5);
+		Button campus = (Button) findViewById(R.id.but_Campus5);
+
+		backgroundView = output; // grafische Ausgabe als Hintergrund setzen
+
+		// OnClickListener für die Buttons:
+		fminus.setOnClickListener(new OnClickListener() {
+			// OnClickListener für F-
+			public void onClick(View v) {
+				fplus.setEnabled(true); // falls F- auf oberstem Stockwerk gedrückt wurde, F+ wieder enablen
+				if (-1 == output.set_floor(0)) // wenn -1 returned wird, unterstes Stockwerk erreicht
+					fminus.setEnabled(false); // F- disabeln
+				output.invalidate(); // redraw
+			}
+		});
+
+		fplus.setOnClickListener(new OnClickListener() {
+			// OnClickListener für F+
+			public void onClick(View v) {
+				fminus.setEnabled(true); // falls F+ auf unterstem Stockwerk gedrückt wurde, F- wieder enablen
+				if (1 == output.set_floor(1)) // wenn 1 returned wird, oberstes Stockwerk erreicht
+					fplus.setEnabled(false); // F+ disabeln
+				output.invalidate(); // redraw
+			}
+		});
+
+		routing.setOnClickListener(new OnClickListener() {
+			// OnClickListener für Routing
+			public void onClick(View v) {
+				launch_state_4();
+			}
+		});
+
+		campus.setOnClickListener(new OnClickListener() {
+			// OnClickListener für Campus
+			public void onClick(View v) {
+				output.set_floor(2);
+				output.invalidate(); // redraw
+			}
+		});
+
+		rl.removeAllViews(); // zunächst müssen alle Views entfernt werden, da sie sonst doppelt vorhanden sind
+		// danach "von hinten nach vorne" die geänderten Elemente wieder hinzufügen, ansonsten würde die Route über den Button dargestellt werden
+		rl.addView(backgroundView);
+		rl.addView(fminus);
+		rl.addView(fplus);
+		rl.addView(routing);
+		rl.addView(campus);
+
+		cl.onResume();
+		// TODO vielleicht lieber in die override, aber momentan gehts auch ohne, da sich die app bei resume noch nicht den status gemerkt hat, also auch onResume und was es sonst noch so gibt mal overriden
 	}
 
-	public void launch_state_6(final View view) {
+	private void launch_state_6() {
 		setContentView(R.layout.state_6);
 		// TODO
 	}
 
-	public void launch_state_7(final View view) {
+	private void launch_state_7() {
 		setContentView(R.layout.state_7);
 		// TODO
 	}
@@ -137,7 +258,7 @@ public class GUI extends Activity {
 			mSensorManager.registerListener(this, Magnet_Sensor, SensorManager.SENSOR_DELAY_GAME);
 		}
 
-		protected void onStop() { // disable Lagesensor
+		protected void onPause() { // disable Lagesensor
 			mSensorManager.unregisterListener(this);
 		}
 
@@ -147,8 +268,8 @@ public class GUI extends Activity {
 				if ((f_new % 5) > 2.5) // Sensor auf 5° Schritte auf bzw. abrunden
 					f_new += 5;
 				f_old = f_new - (f_new % 5); // neuen Winkel merken
-				go.set_degree(f_old); // neuen Winkel an Ausgabe übergeben
-				go.invalidate(); // Bild neu zeichnen
+				output.set_degree(f_old); // neuen Winkel an Ausgabe übergeben
+				output.invalidate(); // Bild neu zeichnen
 			}
 		}
 
